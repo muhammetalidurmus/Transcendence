fd073e9b361940e8c44ea376b33e3b8e23811907
Muhammet Ali Durmuş <94851365+muhammetalidurmus@users.noreply.github.com>
2024-02-28 00:59:30 +0300

    Update login.js

--- frontend/front/jsFile/login.js
+++ frontend/front/jsFile/login.js
@@ -68,7 +68,6 @@ function token(accessToken) { // accessToken parametresini kabul et
         if (xhr.readyState == 4) {
             if (xhr.status == 200) {
                 const data = JSON.parse(xhr.responseText);
-                 console.log(data);
                 if (data) 
                 {
                     localStorage.setItem('username', data.result.login);
